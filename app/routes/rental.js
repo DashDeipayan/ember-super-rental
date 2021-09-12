2ec2452564c0087b663d76be8c018d7801ccfde4
Deipayan Dash <deipayan.dash8@gmail.com>
2021-09-12 06:43:59 +0530

    Ember Data

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,19 +1,10 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Apartment', 'Townhouse'];
+import { inject as service } from '@ember/service';
 
 export default class RentalRoute extends Route {
-  async model(params) {
-    let response = await fetch(`api/rental/${params.rental_id}.json`);
-    let { data } = await response.json();
+  @service store;
 
-    let { id, attributes } = data;
-    let type;
-    if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-      type = 'community';
-    } else {
-      type = 'Standalone';
-    }
-    return { id, type, ...attributes };
+  async model(params) {
+    return this.store.findRecord('rental', params.rental_id);
   }
 }
